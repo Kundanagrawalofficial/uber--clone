46e24da9f6675c07e84d7fa3579cf4a47ce03490
Kundanagrawalofficial <123254080+Kundanagrawalofficial@users.noreply.github.com>
2023-02-09 22:31:22 +0530

    newadding

---
+++ pages/components/map.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useEffect } from 'react'
+import tw from "tailwind-styled-components"
+import mapboxgl from "!mapbox-gl"
+mapboxgl.accessToken=
+'pk.eyJ1Ijoia3VuZGFuYWdyYXdhbCIsImEiOiJjbGR4YTBsaHkwY2hyM3FvYXZicGI1dmpjIn0.OC7q3kt1ad09Uww3IH0fxw'
+
+
+const Map = () => {
+    useEffect(() => {
+        const map = new mapboxgl.Map({
+          container: "map",
+          style: 'mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph',
+          center: [-99.29011,39.39172],
+          zoom: 3,
+        });
+      });
+  return (
+    <Wrapper id='map'>
+
+    </Wrapper>
+  )
+}
+
+export default Map
+const Wrapper=tw.div`
+flex-1
+`
--- pages/index.js
+++ pages/index.js
@@ -2,38 +2,50 @@ import Head from 'next/head'
 import Image from 'next/image'
 // import styles from '@/styles/Home.module.css'
 import tw from "tailwind-styled-components"
-import mapboxgl from "!mapbox-gl"
-import { useEffect } from 'react'
-mapboxgl.accessToken=
-'pk.eyJ1Ijoia3VuZGFuYWdyYXdhbCIsImEiOiJjbGR4YTBsaHkwY2hyM3FvYXZicGI1dmpjIn0.OC7q3kt1ad09Uww3IH0fxw'
+import Map from "./components/Map"
 
 export default function Home() {
-  useEffect(() => {
-    const map = new mapboxgl.Map({
-      container: "map",
-      style: 'mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph',
-      center: [-99.29011,39.39172],
-      zoom: 3,
-    });
-  });
+  
   return (
     <Wrapper>
-      <Map id='map'>Map</Map>
-      <ActionItems>Start</ActionItems>
+      <Map/>
+      <ActionItems>
+        {/* Header */}
+        <Header>
+          <UberLogo src="https://i.ibb.co/84stgjq/uber-technologies-new-20218114.jpg"/>
+          <Profile>
+            <Name>Kundan</Name>
+            <UserImage src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80"/>
+          </Profile>
+        </Header>
+        <ActionButtons>
+
+        </ActionButtons>
+      </ActionItems>
 
     </Wrapper>
   )
 }
 const Wrapper=tw.div`
-flex flex-col bg-red-300 h-screen
-`
-
-const Map=tw.div`
-bg-red-500 flex-1
-
+flex flex-col  h-screen
 `
 
 
 const ActionItems=tw.div`
-flex-1
+flex-1 p-4
+`
+const Header=tw.div`
+flex justify-between item center
+
+`
+const UberLogo= tw.img `
+h-28
+`
+const Profile=tw.div`
+flex items-center`
+const Name=tw.div`
+mr-4 w-20 text-sm`
+const UserImage=tw.img`
+h-12 w-12 rounded-full border border-gray-200 p-px`
+const ActionButtons=tw.div`
 `
