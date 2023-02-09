914382b85d850742136eab62f2ba0a8e0b3ff88e
Kundanagrawalofficial <123254080+Kundanagrawalofficial@users.noreply.github.com>
2023-02-09 21:06:05 +0530

    adding

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '@/styles/Home.module.css'
+import tw from "tailwind-styled-components"
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.map}>Map</div>
+      <div className={styles.start}>Start</div>
+      <div>Start</div>
+
+
+    </div>
+  )
+}
