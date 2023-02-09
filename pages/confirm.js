5dc893be2a388a7297d47c33127a4f9037642218
Kundan Agrawal <123254080+Kundanagrawalofficial@users.noreply.github.com>
2023-02-10 01:53:57 +0530

    cofirm update

--- pages/confirm.js
+++ pages/confirm.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import tw from "tailwind-styled-components"
 import Map from "./components/map"
-const confirm = () => {
-  return (
+const Confirm = () => {
+    const getCoordinates=()=>{
+        const location ="Santa Monica"
+        //good
+        fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?`+
+        new URLSearchParams({
+            accessToken:
+'pk.eyJ1Ijoia3VuZGFuYWdyYXdhbCIsImEiOiJjbGR4YTBsaHkwY2hyM3FvYXZicGI1dmpjIn0.OC7q3kt1ad09Uww3IH0fxw'
+        })
+    )
+        .then(response=> response.json())
+        .then(data=>{
+            console.log(data)
+        })
+    }
+    useEffect(()=>{
+     getCoordinates();
+    },[])
+
+    return(
     <Wrapper>
       <Map/>
       <RideContainer>
+
          Ride Selector
          Confirm Button
       </RideContainer>
@@ -13,7 +32,7 @@ const confirm = () => {
   )
 }
 
-export default confirm
+export default Confirm
 const Wrapper=tw.div`
 flex h-screen flex-col
 `
